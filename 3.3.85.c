f8522b10e5afc90c25ecf858d80f15ac34c728ff
Erdeniz <108977954+erdenizw@users.noreply.github.com>
2022-12-20 20:59:01 +0300

    Create 3.3.85.c

---
+++ 3.3.85.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int main(){
+    int x,y,z,carpim,sayici=10,bolen = 2,q=10;
+    scanf("%d %d", &x, &y);
+    z = x+y;
+    x++;
+    carpim = 0;
+    carpim *= 2;
+    if(sayici > 10)
+        printf("Sayıcı 10'dan büyük.");
+    x--;
+    z-x;
+    z+x;
+    x--;
+    q=q%bolen;
+//    q%=bolen;
+    printf("%.2f\n", 123.32130);
+    printf("%.3f", 3.21312);
+    
+}
