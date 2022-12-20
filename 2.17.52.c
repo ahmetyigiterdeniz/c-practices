c46aae51339a18ece38114af10f83c7f6faef570
Erdeniz <108977954+erdenizw@users.noreply.github.com>
2022-12-20 17:29:34 +0300

    Create 2.17.52.c

---
+++ 2.17.52.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+
+int main(){
+    printf("1\n2\n3\n4\n");
+    for(int i=0; i < 4; ++i)
+        printf("%d\n", i);
+    printf("1");
+    printf("2");
+    printf("3");
+    printf("4");
+}
