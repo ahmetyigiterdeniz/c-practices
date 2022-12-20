13a30009a4c5a5bb4a8eded525c7c00c94ceab50
Erdeniz <108977954+erdenizw@users.noreply.github.com>
2022-12-20 17:15:15 +0300

    Create 2.9.51.c

---
+++ 2.9.51.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main(){
+    printf("İki sayı girin:\n ");
+    int a,b,c;
+    scanf("%d\n", &a);
+    scanf("%d", &b);
+    a=b*c;
+    
+// -----------------------------
+
+    printf("%d", a+b+c);
+    
+}
